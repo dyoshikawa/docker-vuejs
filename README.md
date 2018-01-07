991276674352bc9b958ecf0b8043ee5ca4d84cda
americandog <americandog1993@gmail.com>
2018-01-07 23:26:32 +0900

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+~~~
+npm init -y
+npm install --save-dev webpack babel-cli babel-preset-env babel-loader css-loader vue-template-compiler vue-loader
+npm install --save vue bootstrap
+~~~
---
+++ vuejs/webpack.config.js
@@ -0,0 +1,38 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './dist')
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('./dist/style.css')
+  ]
+};
