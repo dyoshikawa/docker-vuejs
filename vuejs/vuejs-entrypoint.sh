9270b88d00a00bae7dcf9b65a0b660594610591e
amedog <americandog1993@gmail.com>
2018-01-24 00:29:49 +0900

    fix 全体的

--- vuejs/vuejs-entrypoint.sh
+++ vuejs/vuejs-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
-mv /var/www/vuejs_pre/* /var/www/vuejs/
-mv /var/www/vuejs_pre/.??* /var/www/vuejs/
-rmdir /var/www/vuejs_pre
+if [ -f /var/www/vuejs/index.html ]; then
+    sudo rm -rf /var/www/vuejs-pre
+    sudo npm install
+else
+    mv /var/www/vuejs-pre/* /var/www/vuejs/
+    mv /var/www/vuejs-pre/.??* /var/www/vuejs/
+    rmdir /var/www/vuejs_pre
+fi
 chown -R 1000:1000 /var/www/vuejs
 nginx -g 'daemon off;'
